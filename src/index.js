a44e7bb4eb9b71b7137f2e247a5c5fa1999860b5
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-04-05 00:56:40 +0200

    add routes module

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
-import homeController  from './controllers/homeController.js';
+import routes from './routes.js';
+
 
 const app = express();
 const port = 3000;
@@ -12,7 +13,8 @@ app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
 app.use(express.static('public'));
-app.use(homeController);
+app.use(routes)
+
 
 app.listen(port, () => console.log(`Server is listening on http://localhost:${port}`))
 
