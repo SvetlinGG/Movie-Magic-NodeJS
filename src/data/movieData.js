6548644db5ba7b3b8c2414585761e677c8369b79
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-04-08 00:59:55 +0200

    reset db.json

--- src/data/movieData.js
+++ src/data/movieData.js
@@ -4,10 +4,11 @@ import path from 'path';
 
 
 async function getMovies(){
-    const dbPath = path.resolve('./src/db.json')
-    const db = await fs.readFile(dbPath);
+    const dbPath = path.resolve('./src/db.json');
+    const db = await fs.readFile(dbPath, {encoding: 'utf-8'});
+    const data = JSON.parse(db);
 
-    return db.movies;
+    return data.movies;
 }
 export default {
     getMovies,
