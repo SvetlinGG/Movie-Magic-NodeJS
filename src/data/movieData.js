c1d37fc14d2fffb47d676a0503d264cff10a3031
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-04-10 23:25:05 +0200

    setup getDb

--- src/data/movieData.js
+++ src/data/movieData.js
@@ -4,16 +4,17 @@ import path from 'path';
 async function getDb(){
     const dbPath = path.resolve('./src/db.json');
     const db = await fs.readFile(dbPath, {encoding: 'utf-8'});
+    const data = JSON.parse(db);
 
-    return db;
+    return data;
 
 }
 
 async function getMovies(){
+    const db = await getDb();
     
-    const data = JSON.parse(db);
 
-    return data.movies;
+    return db.movies;
 }
 export default {
     getMovies,
