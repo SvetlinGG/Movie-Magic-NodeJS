3c27c0d13876c9e154483dab085d2558731ca4e4
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-04-10 23:22:19 +0200

    getDb function

--- src/data/movieData.js
+++ src/data/movieData.js
@@ -1,11 +1,16 @@
 import fs from 'fs/promises';
 import path from 'path';
 
-
-
-async function getMovies(){
+async function getDb(){
     const dbPath = path.resolve('./src/db.json');
     const db = await fs.readFile(dbPath, {encoding: 'utf-8'});
+
+    return db;
+
+}
+
+async function getMovies(){
+    
     const data = JSON.parse(db);
 
     return data.movies;
